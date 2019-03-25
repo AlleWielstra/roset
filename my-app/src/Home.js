3b6ade5f989a17a2753a4ddc2c4aa99056527517
ALLE_HOME <allegijsbertwielstra@gmail.com>
2019-03-25 14:53:28 +0100

    installing something new commiting in case revert is needing

---
+++ my-app/src/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <div className="card">
+                    <div className="card-header">
+                        specialiteiten deze week
+                    </div>
+                    <div className="card-content">
+                        Carousel here<br/><br/><br/><br/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
---
+++ my-app/src/Main.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import {
+    Route,
+    NavLink,
+    HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import Ijs from "./Ijs";
+import Contact from "./Contact";
+
+class Main extends Component {
+    render() {
+        return (
+            <HashRouter>
+                <div>
+                    <ul className="header">
+                        <li className="store-name">De Roset</li>
+                        <li><NavLink exact to="/">Home</NavLink></li>
+                        <li><NavLink to="/ijs">Ijs</NavLink></li>
+                        <li><NavLink to="/contact">Contact</NavLink></li>
+                    </ul>
+                    <div className="content">
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/Ijs" component={Ijs}/>
+                        <Route path="/contact" component={Contact}/>
+                    </div>
+                    <ul className="footer">
+                        <li>De Roset voor premium ijs</li>
+                    </ul>
+                </div>
+            </HashRouter>
+        );
+    }
+}
+
+export default Main;
